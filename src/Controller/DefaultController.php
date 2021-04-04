12382955a66c6f82e63de17dd9c13e123f777cb9
sasa-bonta <bonta2000@mail.ru>
2021-04-04 21:46:53 +0300

    Vasile mege bine Vasile

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 
 namespace App\Controller;
 
+// http://localhost:8000/products
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
