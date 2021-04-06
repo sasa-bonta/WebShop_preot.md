a2b04911151ac42f24055248043fffa2f30b0c82
sasa-bonta <bonta2000@mail.ru>
2021-04-06 20:13:11 +0300

    Content passes over header

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,10 +3,10 @@
 
 namespace App\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+// http://localhost:8000
 class DefaultController extends AbstractController
 {
     /**
