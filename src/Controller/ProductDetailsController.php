8a409aeda6307009c839c62a7934d0e5c6dcf800
sasa-bonta <bonta2000@mail.ru>
2021-04-07 23:35:25 +0300

    Should add galery

--- src/Controller/ProductDetailsController.php
+++ src/Controller/ProductDetailsController.php
@@ -17,3 +17,29 @@ class ProductDetailsController extends AbstractController
         return $this->render('product-details/index.html.twig');
     }
 }
+
+
+//<div class="row">
+//                <div class="column">
+//                    <img src="{{ asset('assets/main/images/product1.jpg') }}" alt="Nature" style="width:100%"
+//                         onclick="myFunction(this);">
+//                </div>
+//                <div class="column">
+//                    <img src="{{ asset('assets/main/images/product1.jpg') }}" alt="Snow" style="width:100%"
+//                         onclick="myFunction(this);">
+//                </div>
+//                <div class="column">
+//                    <img src="{{ asset('assets/main/images/product1.jpg') }}" alt="Mountains" style="width:100%"
+//                         onclick="myFunction(this);">
+//                </div>
+//                <div class="column">
+//                    <img src="{{ asset('assets/main/images/product1.jpg') }}" alt="Lights" style="width:100%"
+//                         onclick="myFunction(this);">
+//                </div>
+//            </div>
+//
+//            <div class="container">
+//                <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span>
+//                <img id="expandedImg" style="width:100%">
+//                <div id="imgtext"></div>
+//            </div>
