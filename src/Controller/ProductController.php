9bd288480dd32f2167493c42f89605c7309d53c1
sasa-bonta <bonta2000@mail.ru>
2021-04-09 16:16:37 +0300

    Show by code, edit by code, delete by code work well

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -69,7 +69,7 @@ class ProductController extends AbstractController
     }
 
     /**
-     * @Route("/{id}/edit", name="product_edit", methods={"GET","POST"})
+     * @Route("/{code}/edit", name="product_edit", methods={"GET","POST"})
      */
     public function edit(Request $request, Product $product): Response
     {
@@ -94,11 +94,11 @@ class ProductController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="product_delete", methods={"POST"})
+     * @Route("/{code}", name="product_delete", methods={"POST"})
      */
     public function delete(Request $request, Product $product): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete'.$product->getCode(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($product);
             $entityManager->flush();
