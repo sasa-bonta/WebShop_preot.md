04aa1049483d83013bc0669b010ce12b2339379c
sasa-bonta <bonta2000@mail.ru>
2021-04-09 04:26:45 +0300

    New Table product created

--- migrations/Version20210409012146.php
+++ migrations/Version20210409012146.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210409011057 extends AbstractMigration
+final class Version20210409012146 extends AbstractMigration
 {
     public function getDescription() : string
     {
