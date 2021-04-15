98b231666dbb200860e733a24e835f23cdc9e55d
sasa-bonta <bonta2000@mail.ru>
2021-04-15 18:35:50 +0300

    Merge branch 'sasa'

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,27 +19,38 @@ class DefaultController extends AbstractController
         $name = null;
         $category = null;
         $page = 1;
-        $limit = 10;
+        $limit = 16;
         $orderBy = 'created_at:ASC';
+        $request = Request::createFromGlobals();
+        $request->getPathInfo();
 
-        if (isset($_GET['name'])) {
-            $name = $_GET['name'];
+        if ($request->query->get('name')) {
+            $name = $request->query->get('name');
         }
-        if (isset($_GET['category'])) {
-            $category = $_GET['category'];
+        if ($request->query->get('category')) {
+            $category = $request->query->get('category');
         }
-        if (isset($_GET['page'])) {
-            $page = $_GET['page'];
+        if ($request->query->get('page')) {
+            $page = $request->query->get('page');
         }
-        if (isset($_GET['limit'])) {
-            $limit = $_GET['limit'];
+        if ($request->query->get('limit')) {
+            $limit = $request->query->get('limit');
         }
-        if (isset($_GET['order'])) {
-            $orderBy = $_GET['order'];
+        if ($request->query->get('order')) {
+            $orderBy = $request->query->get('order');
         }
+        # Catch and display errors
+//        if ($limit <= 0) {
+//            throw new Exception('Limit must be positive');
+//        }
+//        if ($page <= 0) {
+//            throw new Exception('Number of page must be positive');
+//        }
         $offset = ($page - 1) * $limit;
-        return $this->render('main/index.html.twig', [
+        return $this->render('main/product/index.html.twig', [
             'products' => $productRepository->search($name, $category, $orderBy, $limit, $offset),
+            'length' => $productRepository->countTotal($name, $category),
+            'limit' => $limit
         ]);
     }
 }
