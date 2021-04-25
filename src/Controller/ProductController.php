16108e33475a4e37e99261470de2ac0563cf4347
sasa-bonta <bonta2000@mail.ru>
2021-04-25 12:01:02 +0300

    Admin crud pages

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -65,54 +65,6 @@ class ProductController extends AbstractController
         ]);
     }
 
-    function generateRandomString($length = 10)
-    {
-        return substr(str_shuffle(str_repeat($x = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length / strlen($x)))), 1, $length);
-    }
-
-    /**
-     * @Route("/new", name="product_new", methods={"GET","POST"})
-     */
-    public function new(Request $request): Response
-    {
-        $product = new Product();
-        $form = $this->createForm(ProductType::class, $product);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            # Errors solve later
-//            $repo = $this->getDoctrine()->getRepository(Product::class);
-//            if ($repo->count(['code'=>$product->getCode()]) > 0) {
-//                # code 400, display alert. Return this
-//                return $this->render('product/new.html.twig', [
-//                    'errors' => ['duplicated code'],
-//                    'product' => $product,
-//                    'form' => $form->createView(),
-//                ]);
-//            }
-            $entityManager = $this->getDoctrine()->getManager();
-            $dateTime = new DateTime(null, new DateTimeZone('Europe/Athens'));
-            # Random product generator. To make it work comment all the fields excluding code from ProductType
-//            $categ = ['cars', 'toys', 'supplies', 'tools'];
-//            $product->setName('product' .rand(0, 30));
-//            $product->setCode($this->generateRandomString());
-//            $product->setCategory($categ[rand(0,3)]);
-//            $product->setPrice(rand(1, 1601));
-//            $product->setDescription('Lorem ipsum dolor sit amet, consectetuer adipiscing elit. ');
-//            $product->setImgPath('assets/main/images/product1.jpg');
-            $product->setCreatedAt($dateTime);
-            $entityManager->persist($product);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('product_index');
-        }
-
-        return $this->render('main/product/new.html.twig', [
-            'product' => $product,
-            'form' => $form->createView(),
-        ]);
-    }
-
     /**
      * @Route("/{code}", name="product_show", methods={"GET"})
      */
@@ -121,40 +73,5 @@ class ProductController extends AbstractController
         return $this->render('main/product/product_details.html.twig', [
             'product' => $product,
         ]);
-    }
-
-    /**
-     * @Route("/{code}/edit", name="product_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Product $product): Response
-    {
-        $form = $this->createForm(ProductType::class, $product);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $dateTime = new DateTime(null, new DateTimeZone('Europe/Athens'));
-            $product->setUpdatedAt($dateTime);
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('product_index');
-        }
-
-        return $this->render('main/product/edit.html.twig', [
-            'product' => $product,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{code}", name="product_delete", methods={"POST"})
-     */
-    public function delete(Request $request, Product $product): Response
-    {
-        if ($this->isCsrfTokenValid('delete' . $product->getCode(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($product);
-            $entityManager->flush();
-        }
-        return $this->redirectToRoute('product_index');
     }
 }
