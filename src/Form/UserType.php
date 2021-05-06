8504431b76f4e86d5d972a6aabc773e465a5b7cf
sasa-bonta <bonta2000@mail.ru>
2021-05-06 20:57:46 +0300

    Parca gata

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -19,7 +19,12 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', TextType::class)
+            ->add('username', TextType::class, [
+                'constraints' => new Length([
+                    'min' => 1,
+                    'max' => 35
+                ])
+            ])
             ->add('email', EmailType::class)
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
