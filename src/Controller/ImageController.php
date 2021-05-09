4c4914c77ca4168aae11dae54e2668a33e607595
sasa-bonta <bonta2000@mail.ru>
2021-05-09 04:08:11 +0300

    Tags validation

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -6,9 +6,14 @@ use App\Entity\Image;
 use App\Form\ImageType;
 use App\Repository\ImageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @Route("/admin/galery")
@@ -28,13 +33,63 @@ class ImageController extends AbstractController
     /**
      * @Route("/new", name="image_new", methods={"GET","POST"})
      */
-    public function new(Request $request): Response
+    public function new(Request $request, SluggerInterface $slugger): Response
     {
         $image = new Image();
         $form = $this->createForm(ImageType::class, $image);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile $imageFile */
+
+            $tags = mb_strtolower($image->getTags());
+            $errors = [];
+
+            if (preg_match('/[^a-zа-я0-9]/', $tags)) {
+                array_push($errors, "The tags must contain only characters and digits");
+            }
+
+            $tags = explode(',', $tags);
+            $trimmedTags = [];
+            foreach ($tags as $tag) {
+                $tag = ltrim($tag);
+                $tag = rtrim($tag);
+                array_push($trimmedTags, $tag);
+            }
+
+            foreach ($trimmedTags as $tag) {
+                if (mb_strlen($tag) > 12 || mb_strlen($tag) < 2) {
+                    array_push($errors, "The length of each tag must be from 2 to 12 characters");
+                }
+            }
+
+            if (!empty($errors)) {
+                return $this->render('admin/image/new.html.twig', [
+                    'errors' => $errors,
+                    'image' => $image,
+                    'form' => $form->createView(),
+                ]);
+            }
+
+            $imageFile = $form->get('path')->getData();
+            $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+            // this is needed to safely include the file name as part of the URL
+            $safeFilename = $slugger->slug($originalFilename);
+            $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+            // Move the file to the directory where brochures are stored
+            try {
+                $gallery = $this->getParameter('gallery_path');
+                $imageFile->move(
+                    $gallery,
+                    $newFilename
+                );
+            } catch (FileException $e) {
+                throw new BadRequestHttpException($e->getMessage());
+            }
+            // updates the 'brochureFilename' property to store the PDF file name
+            // instead of its contents
+            $image->setTags(json_encode($trimmedTags));
+            $image->setPath($newFilename);
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($image);
             $entityManager->flush();
@@ -61,8 +116,12 @@ class ImageController extends AbstractController
     /**
      * @Route("/{id}/edit", name="image_edit", methods={"GET","POST"})
      */
+    # @fixme edit image
     public function edit(Request $request, Image $image): Response
     {
+//        $gallery = $this->getParameter('gallery_path');
+//        $image->setPath($gallery .$image->getPath());
+//        $image->setPath(new File($image->getPath()));
         $form = $this->createForm(ImageType::class, $image);
         $form->handleRequest($request);
 
@@ -83,7 +142,7 @@ class ImageController extends AbstractController
      */
     public function delete(Request $request, Image $image): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$image->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $image->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($image);
             $entityManager->flush();
--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Image;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +14,13 @@ class ImageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('tags')
-            ->add('path')
-            ->add('description')
-        ;
+            ->add('tags', TextType::class, [
+                'attr' => array(
+                    'placeholder' => 'Separate tags with commas'
+                )
+            ])
+            ->add('path', FileType::class)
+            ->add('description');
     }
 
     public function configureOptions(OptionsResolver $resolver)
