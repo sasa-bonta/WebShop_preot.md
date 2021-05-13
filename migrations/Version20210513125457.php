123869d1c5783b9e283493cdb6e4b54a646ef04c
sasa-bonta <bonta2000@mail.ru>
2021-05-13 17:42:51 +0300

    On button click displays fragment

--- migrations/Version20210513125457.php
+++ migrations/Version20210513125457.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210505134032 extends AbstractMigration
+final class Version20210513125457 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,14 @@ final class Version20210505134032 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD activated TINYINT(1) DEFAULT \'0\' NOT NULL');
+        $this->addSql('CREATE TABLE image (id INT AUTO_INCREMENT NOT NULL, tags VARCHAR(255) NOT NULL, path VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE product DROP available_amount');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP activated');
+        $this->addSql('DROP TABLE image');
+        $this->addSql('ALTER TABLE product ADD available_amount INT DEFAULT 0 NOT NULL');
     }
 }
