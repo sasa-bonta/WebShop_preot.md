2097bdee5d3fb3387787b7d72c930331a1aed9bc
sasa-bonta <bonta2000@mail.ru>
2021-05-15 18:56:36 +0300

    Amin

--- public/assets/main/cart/cart.js
+++ public/assets/main/cart/cart.js
@@ -15,7 +15,13 @@ function readItemsTemplate(data) {
              </tr>`;
     }
 
+    var formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
+
     data.forEach(cartItem => {
+
         var icon = JSON.parse(cartItem['product']['imgPath']);
         total += cartItem['product']['price'] * cartItem['amount'];
 
@@ -23,9 +29,9 @@ function readItemsTemplate(data) {
         <tr>
             <td style="text-align: center"><img src="/assets/main/images/gallery/` + icon[0] + `" alt="img" width="100" height="100"></td>
             <td style="vertical-align: middle;text-align: center">` + cartItem['product']['name'] + `</td>
-            <td style="vertical-align: middle;text-align: center">` + cartItem['product']['price'] + ` $</td>
+            <td style="vertical-align: middle;text-align: center">` + formatter.format(cartItem['product']['price']) + ` $</td>
             <td style="vertical-align: middle;text-align: center">` + cartItem['amount'] + `</td>
-            <td style="vertical-align: middle;text-align: center">` + cartItem['amount'] * cartItem['product']['price'] + ` $</td>
+            <td style="vertical-align: middle;text-align: center">` + formatter.format(cartItem['amount'] * cartItem['product']['price']) + ` $</td>
             <td style="vertical-align: middle;text-align: center"><button class="delete-item-btn btn btn-danger" data-code="` + cartItem['product']['code'] + `">X</button></td>
         </tr>
         `;
@@ -38,7 +44,7 @@ function readItemsTemplate(data) {
         read_items_html += `
             <tr>
                 <td style="vertical-align: middle;text-align: right" colspan="4"><b>Total : </b></td>
-                <td style="vertical-align: middle;text-align: center"><b>` + total + ` $</b></td>
+                <td style="vertical-align: middle;text-align: center"><b>` + formatter.format(total) + ` $</b></td>
                 <td></td>
             </tr>
         </table>`;
