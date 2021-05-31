34a3a8bce82b83f5cf0bf190c7033f6f23d5a0f4
sasa-bonta <bonta2000@mail.ru>
2021-05-31 17:54:20 +0300

    Lapo4ka

--- src/Controller/ProductApiController.php
+++ src/Controller/ProductApiController.php
@@ -95,12 +95,7 @@ class ProductApiController extends AbstractController
         $initCode = $product->getCode();
         $form = $this->createForm(ProductType::class, $product, ['csrf_protection' => false]);
         $form->handleRequest($request);
-        $keys = ['code', 'name', 'category', 'price', 'availableAmount', 'description'];
-        foreach ($keys as $key) {
-            if (!array_key_exists($key, $parameters) || empty($parameters[$key])) {
-                throw new BadRequestHttpException($form->getErrors(true, true));
-            }
-        }
+
         $form->submit($parameters);
 
         if ($form->isSubmitted() && $form->isValid()) {
