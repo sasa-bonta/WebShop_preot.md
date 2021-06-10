61b62524d53df257052157324b0254e242dbf58e
sasa-bonta <bonta2000@mail.ru>
2021-06-11 01:38:39 +0300

    modal displayed once

--- public/assets/main/order/order.js
+++ public/assets/main/order/order.js
@@ -19,12 +19,12 @@ $("#order_payment_type_1").on('click', function () {
     $('#order_payment_card_expiresAt').attr("required", true);
 });
 
-const urlParams = new URLSearchParams(window.location.search);
+let urlParams = new URLSearchParams(window.location.search);
 if (urlParams.has("success")) {
     const myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
         backdrop: true
     })
     myModal.show();
+    window.history.replaceState({}, document.title, location.protocol + '//' + location.host + location.pathname);
 }
-urlParams.delete("success");
 
