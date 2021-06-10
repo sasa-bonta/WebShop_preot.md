345a0d6343c14dab46630bcf57a48cf1129be399
sasa-bonta <bonta2000@mail.ru>
2021-06-10 10:16:12 +0300

    cash/card required card details

--- public/assets/main/order/order.js
+++ public/assets/main/order/order.js
@@ -7,8 +7,13 @@ $("#checkout").on('click', function () {
 
 $("#order_payment_type_0").on('click', function () {
     $('#card-details').hide();
+    $('#order_payment_card_code').attr("required", false);
+    $('#order_payment_card_cvv').attr("required", false);
 });
 
 $("#order_payment_type_1").on('click', function () {
     $('#card-details').show();
+    $('#order_payment_card_code').attr("required", true);
+    $('#order_payment_card_cvv').attr("required", true);
+    // $('')
 });
